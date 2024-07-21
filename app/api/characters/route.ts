173834269fb4f8416be379aa22f584484a156905
Yuri Yang <62859796+itsyurika@users.noreply.github.com>
2024-07-21 17:53:31 -0400

    add eslint config and delete unnecessary folder structure (#16)

--- app/api/characters/route.ts
+++ app/api/characters/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server";
-import prisma from "../../../lib/prisma";
+import { NextResponse } from 'next/server';
+import prisma from '../../../lib/prisma';
 
 export async function GET() {
   const user = await prisma.user.findFirst();
