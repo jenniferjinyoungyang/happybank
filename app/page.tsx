914bce9bff69383e5bb10f5cec235c6575ceae3b
Yuri Yang <62859796+itsyurika@users.noreply.github.com>
2024-07-21 18:56:34 -0400

    update css styling (#19)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello Happybank</h1>
+    <main>
+      <h1 className="text-lg font-bold underline">Hello Happybank</h1>
     </main>
   );
 }
