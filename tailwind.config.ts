4f12d6f504e53cf8d695e367c8f663915553962b
Jennifer Yang <jennifer.jinyoung.yang@gmail.com>
2024-08-18 19:19:58 -0400

    style login screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,14 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: ['app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
-    plugins: [],
+    extend: {
+      backgroundImage: {
+        'login-image':
+          "url('https://cdn.pixabay.com/photo/2017/06/07/12/56/sunset-2380279_1280.jpg')",
+      },
+      plugins: [],
+    },
   },
 };
+
 export default config;
