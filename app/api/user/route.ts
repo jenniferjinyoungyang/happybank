9873f623ae9d11804ec6375a6d17a1d34e53c8b4
YuriMacBook <07yuri@gmail.com>
2024-09-02 16:11:52 -0400

    add create user api path

---
+++ app/api/user/route.ts
@@ -0,0 +1,54 @@
+import { NextResponse } from 'next/server';
+import prisma from '../../../lib/prisma';
+import { hash } from 'bcrypt';
+import * as z from 'zod';
+
+// Define a schema for input validation
+const userSchema = z.object({
+  name: z.string().min(1, 'Username is required').max(50),
+  email: z.string().min(1, 'Email is required').email('Invalid email'),
+  password: z
+    .string()
+    .min(1, 'Password is required')
+    .min(6, 'Password must have 6 characters'),
+});
+
+export async function Post(req: Request) {
+  try {
+    const body = await req.json();
+    const { email, name, password } = userSchema.parse(body);
+
+    //check if email already exists
+    const existingUserByEmail = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (existingUserByEmail) {
+      return NextResponse.json(
+        { user: null, message: 'User with this email already exists' },
+        { status: 409 }
+      );
+    }
+
+    const hashedPassword = await hash(password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+      },
+    });
+
+    const { password: newUserPassword, ...rest } = newUser;
+
+    return NextResponse.json(
+      { user: rest, message: 'User created successfully' },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'Oops! Something went wrong :(' },
+      { status: 500 }
+    );
+  }
+}
