dac3a496348f63d261c5d83ad8df52b4d515a9b4
Yuri Yang <62859796+itsyurika@users.noreply.github.com>
2024-09-02 17:32:44 -0400

    add create user api endpoint (#30)

--- app/api/characters/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import prisma from '../../../lib/prisma';
-
-export async function GET() {
-  const user = await prisma.user.findFirst();
-  return NextResponse.json({ characters: user?.email });
-}
