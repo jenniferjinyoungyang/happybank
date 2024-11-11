5a910e3a715403c89104bfa42c2d512a457521f5
Jennifer Yang <jennifer.jinyoung.yang@gmail.com>
2024-11-11 16:49:18 -0500

    adds dashboard header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import SessionWrapper from './components/SessionWrapper';
+import SessionWrapper from './_components/SessionWrapper';
 
 const inter = Inter({ subsets: ['latin'] });
 
