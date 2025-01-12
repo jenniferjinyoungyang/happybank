d3dbc6b05cdf60a9b5853164c203ea9e0e42b299
Jennifer Yang <jennifer.jinyoung.yang@gmail.com>
2025-01-12 16:26:34 -0500

    add sign up page

--- app/sign-up/_components/SignUp.tsx
+++ app/sign-up/_components/SignUp.tsx
@@ -7,8 +7,8 @@ import { Button } from '../../_shared/_components/Button';
 import { GoogleLogo } from '../../_shared/_components/icons/GoogleLogo';
 import { HappyBankLogo } from '../../_shared/_components/icons/HappyBankLogo';
 
-const SignIn: React.FC = () => {
-  const { register, handleSubmit } = useForm();
+export const SignUp: React.FC = () => {
+  const { register } = useForm();
 
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
@@ -19,45 +19,35 @@ const SignIn: React.FC = () => {
         <div className="h-screen flex w-1/2 p-6 sm:p-12">
           <div className="flex flex-col items-center inline-block align-middle w-2/3 m-auto">
             <HappyBankLogo />
-            <h2 className="mt-16">Welcome to Happy bank!</h2>
+            <h2 className="my-16">Create an account</h2>
             <div className="max-w-lg w-full flex-1 my-8">
-              <div className="flex flex-col items-center w-3/4 m-auto">
-                <button
-                  type="button"
-                  onClick={() => signIn('google', { callbackUrl: '/dashboard' })}
-                  className="w-full inline-flex items-center justify-center py-4 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-gray-900 focus:z-10 focus:ring-4 focus:ring-gray-200"
-                >
-                  <GoogleLogo />
-                  Continue with Google
-                </button>
-
-                <div className="w-full mt-6 mb-12 border-b text-center">
-                  <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                    Or
-                  </div>
-                </div>
-              </div>
-
               <div className="w-3/4 mx-auto">
-                <form
-                  onSubmit={handleSubmit(async ({ email, password }) => {
-                    signIn('credentials', {
-                      email,
-                      password,
-                      callbackUrl: '/dashboard',
-                    });
-                  })}
-                >
+                <form>
                   <div className="relative">
+                    <input
+                      className="block p-4 w-full text-sm text-gray-900 bg-gray-100 rounded-lg appearance-none focus:outline-none focus:ring-0 focus:border-0 peer"
+                      {...register('fullName')}
+                      type="full-name"
+                      id="full-name-sign-up"
+                      placeholder=""
+                    />
+                    <label
+                      htmlFor="full-name-sign-up"
+                      className="absolute text-sm text-gray-500 font-medium duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-placeholder-shown:px-4 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                    >
+                      Full Name
+                    </label>
+                  </div>
+                  <div className="relative mt-5">
                     <input
                       className="block p-4 w-full text-sm text-gray-900 bg-gray-100 rounded-lg appearance-none focus:outline-none focus:ring-0 focus:border-0 peer"
                       {...register('email')}
                       type="email"
-                      id="email-sign-in"
+                      id="email-sign-up"
                       placeholder=""
                     />
                     <label
-                      htmlFor="email-sign-in"
+                      htmlFor="email-sign-up"
                       className="absolute text-sm text-gray-500 font-medium duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-placeholder-shown:px-4 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
                     >
                       Email
@@ -69,23 +59,38 @@ const SignIn: React.FC = () => {
                       className="block p-4 w-full text-sm text-gray-900 bg-gray-100 rounded-lg appearance-none focus:outline-none focus:ring-0 focus:border-0 peer"
                       {...register('password')}
                       type="password"
-                      id="password-sign-in"
+                      id="password-sign-up"
                       placeholder=""
                     />
                     <label
-                      htmlFor="password-sign-in"
+                      htmlFor="password-sign-up"
                       className="absolute text-sm text-gray-500 font-medium duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-placeholder-shown:px-4 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
                     >
                       Password
                     </label>
                   </div>
-                  <Button type="submit" label="Sign in" cssWrapper="w-full mt-16" />
+                  <Button type="submit" label="Create an account" cssWrapper="w-full mt-16" />
                 </form>
+              </div>
 
+              <div className="flex flex-col items-center w-3/4 m-auto">
+                <div className="w-full mt-6 mb-12 border-b text-center">
+                  <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                    Or
+                  </div>
+                </div>
+                <button
+                  type="button"
+                  onClick={() => signIn('google', { callbackUrl: '/dashboard' })}
+                  className="w-full inline-flex items-center justify-center py-4 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-gray-900 focus:z-10 focus:ring-4 focus:ring-gray-200"
+                >
+                  <GoogleLogo />
+                  Continue with Google
+                </button>
                 <p className="mt-6 text-xs text-gray-600 text-center">
-                  Don&apos;t have an account?
-                  <Link href="/sign-up" className="ml-1 border-b border-gray-500 border-dotted">
-                    Sign up
+                  Already have an account?
+                  <Link href="/sign-in" className="ml-1 border-b border-gray-500 border-dotted">
+                    Sign in
                   </Link>
                 </p>
               </div>
@@ -96,5 +101,3 @@ const SignIn: React.FC = () => {
     </div>
   );
 };
-
-export default SignIn;
